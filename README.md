333e72e5ded9f09c94f0a44e3f8499e4528725e2
vitasunnie0812 <lsunnyl89@gmail.com>
2020-01-10 21:32:44 +0900

    deleted

--- README.md
+++
@@ -1,6 +0,0 @@
-###README.md
-
-- readme file 입니다.
-- MarkDown 파일이라 문법이 존재합니다.
-- https://gist.github.com/ihoneymon/652be052a0727ad59601
-- 위 링크레서 문법 확인 가능 합니다.
--- wsgi.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-application = Flask(__name__)
-
-@application.route("/")
-def hello():
-    return "OpenShift Hello KPST World!"
-
-if __name__ == "__main__":
-    application.run()
